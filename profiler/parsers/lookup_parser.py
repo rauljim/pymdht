1f794863e2140c61ddd80d5f4aeb058eb3ac2480
Raul Jimenez <rauljc@kth.se>
2011-01-06 18:11:12 +0100

    added parsers

---
+++ profiler/parsers/lookup_parser.py
@@ -0,0 +1,117 @@
+# Copyright (C) 2010 Raul Jimenez
+# Released under GNU LGPL 2.1
+# See LICENSE.txt for more information
+
+"""
+
+
+"""
+
+import core.message as message
+
+import cdf
+
+class LookupInfo(object):
+
+    def __init__(self, info_hash, start_ts):
+        self.info_hash = info_hash
+        self.start_ts = start_ts
+
+        self.num_queries = 0
+        self.num_queries_till_peers = 0
+        self.ts_peers = []
+        self.peer_set = set()
+        self.closest_log_distance = 160
+        self.ctime = 0
+
+class Parser(object):
+
+    def __init__(self, label, my_addr):
+        self.label = label
+        self.my_addr = my_addr
+
+        self.tids = {} # Key is tid[0] and value is the (addr, lookup)
+                       # associated
+        self.lookups = {}
+
+    def outgoing_msg(self, ts, dst_addr, msg):
+        if msg.type == message.QUERY:
+            if msg.query == message.GET_PEERS:
+                # lookup query
+                lookup = self.lookups.get(msg.info_hash, None)
+                if not lookup:
+                    lookup = LookupInfo(msg.info_hash, ts)
+                    self.lookups[msg.info_hash] = lookup
+                self.tids[msg.tid[0]] = (dst_addr, lookup)
+                lookup.num_queries += 1
+                if not lookup.ts_peers:
+                    lookup.num_queries_till_peers += 1
+
+    def incoming_msg(self, ts, src_addr, msg):
+        if msg.type != message.RESPONSE:
+            return
+        peers = getattr(msg, 'peers', None)
+        if not peers:
+            return
+        # This is a response WITH peers
+        try:
+            addr, lookup = self.tids[msg.tid[0]]
+        except (KeyError):
+            print 'response with peers to unknown query'
+            return
+        del self.tids[msg.tid[0]]
+        if addr == src_addr:
+            lookup.ts_peers.append((ts, peers))
+            for peer in peers:
+                lookup.peer_set.add(peer)
+            log_distance =  lookup.info_hash.log_distance(msg.sender_id)
+            if log_distance < lookup.closest_log_distance:
+                lookup.closest_log_distance = log_distance
+                lookup.ctime = ts - lookup.start_ts
+        
+
+
+    def done(self):
+        num_lookups_without_peeers = 0
+        ctime_file = open(self.label + '.l_ctime', 'w')
+        c_ld_file = open(self.label + '.l_c_ld', 'w')
+        time_file = open(self.label + '.l_time', 'w')
+        queries_file = open(self.label + '.l_queries', 'w')
+        peers_time_file = open(self.label + '.l_peers_time', 'w')
+        num_peers_file = open(self.label + '.l_num_peers', 'w')
+        swarm_size_file = open(self.label + '.l_swarm_size', 'w')
+        queries_till_peers_file = open(
+            self.label + '.l_queries_till_peers', 'w')
+        lookup_times = []
+        lookup_queries = []
+        for lookup in self.lookups.values():
+            for ts, peers in lookup.ts_peers:
+                num_peers_file.write('%d ' % len(peers))
+                peers_time_file.write('%f ' % (ts - lookup.start_ts))
+            if lookup.ts_peers:
+                lookup_time = lookup.ts_peers[0][0] - lookup.start_ts
+                lookup_times.append(lookup_time)
+                time_file.write('%f\n' % (lookup_time)) 
+                num_peers_file.write('\n')
+                peers_time_file.write('\n')
+                swarm_size_file.write('%d\n' % (len(lookup.peer_set)))
+            else:
+                num_lookups_without_peeers += 1
+                
+            lookup_queries.append(lookup.num_queries)
+            queries_file.write('%d\n' % (lookup.num_queries))
+            queries_till_peers_file.write('%d\n' % (
+                    lookup.num_queries_till_peers))
+            ctime_file.write('%.4f\n' % (lookup.ctime))
+            c_ld_file.write('%d\n' % (lookup.closest_log_distance))
+                              
+        time_cdf_file = open(self.label + '.l_time.cdf', 'w')
+        queries_cdf_file = open(self.label + '.l_queries.cdf', 'w')
+        for cum, value in cdf.cdf(lookup_times):
+            time_cdf_file.write('%.4f\t%.4f\n' % (cum, value))
+        for cum, value in cdf.cdf(lookup_queries):
+            queries_cdf_file.write('%.4f\t%d\n' % (cum, value))
+
+        
+        print '%s: No peers for %d lookups' % (self.label,
+                                              num_lookups_without_peeers)
---
+++ profiler/parsers/rtt_parser.py
@@ -0,0 +1,91 @@
+# Copyright (C) 2010 Raul Jimenez
+# Released under GNU LGPL 2.1
+# See LICENSE.txt for more information
+
+"""
+Prints the lookup time in seconds (one lookup per line).
+The output is not chronologically sorted.
+
+"""
+import core.message as message
+
+
+class QueryInfo(object):
+
+    def __init__(self, ts, dst_addr, is_lookup):
+        self.ts = ts
+        self.dst_addr = dst_addr
+        self.is_lookup = is_lookup
+
+class Parser(object):
+
+    def __init__(self, label, my_addr):
+        self.label = label
+        self.my_addr = my_addr
+
+        self.num_l_q = 0
+        self.num_m_q = 0
+        self.num_l_r = 0
+        self.num_m_r = 0
+
+        self.num_l_wport = 0
+        self.num_m_wport = 0
+        
+        self.l_rtt_file = open(label + '.l_rtt', 'w')
+        self.m_rtt_file = open(label + '.m_rtt', 'w')
+        self.t_rtt_file = open(label + '.t_rtt', 'w')
+
+        self.tids = {}
+
+    def outgoing_msg(self, ts, dst_addr, msg):
+            
+        if msg.type == message.QUERY:
+            if msg.query == message.GET_PEERS:
+                # lookup query
+                self.num_l_q += 1
+                self.tids[msg.tid[0]] = query = QueryInfo(ts, dst_addr, True)
+            else:
+                # non-lookoup query
+                self.num_m_q += 1
+                self.tids[msg.tid[0]] = QueryInfo(ts, dst_addr, False)
+                
+    def incoming_msg(self, ts, src_addr, msg):
+        if msg.type == message.RESPONSE:
+            try:
+                related_query = self.tids[msg.tid[0]]
+            except (KeyError):
+                print '%s: rtt_parser: no query for this response' % (
+                    self.label)
+                return
+            if src_addr != related_query.dst_addr:
+                if src_addr[0] != related_query.dst_addr[0]:
+                    print '%s: rtt_parser: different IP: %r != %r %f' % (
+                        self.label, related_query.dst_addr, src_addr,
+                        ts - related_query.ts
+                        )
+                else:
+                    del self.tids[msg.tid[0]]
+                    if related_query.is_lookup:
+                        self.num_l_wport += 1
+                    else:
+                        self.num_m_wport += 1
+                return
+            
+            # addr and tid matched
+            if related_query.is_lookup:
+                self.num_l_r += 1
+                self.l_rtt_file.write('%f\n' % (ts - related_query.ts))
+            else:
+                self.num_m_r += 1
+                self.m_rtt_file.write('%f\n' % (ts - related_query.ts))
+            self.t_rtt_file.write('%r\t%f\n' % (src_addr[0], ts - related_query.ts))
+
+    def done(self):
+        self.l_q_r_wp_file = open(self.label + '.l_q_r_wp', 'w')
+        self.l_q_r_wp_file.write('%d\t%d\t%d\n' % (
+                self.num_l_q, self.num_l_r, self.num_l_wport))
+        self.m_q_r_wp_file = open(self.label + '.m_q_r_wp', 'w') 
+        self.m_q_r_wp_file.write('%d\t%d\t%d\n' % (
+                self.num_m_q, self.num_m_r, self.num_m_wport))
+    
+                              
