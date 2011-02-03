2b1eb064d4b8021f10aa9281b1e72d9b7f17823d
Raul Jimenez <rauljim@gmail.com>
2011-02-03 18:06:00 +0100

    size estimation modified to get partial results over time

--- size_estimation.py
+++ size_estimation.py
@@ -1,19 +1,37 @@
 from core.identifier import Id
 
-sizes = []
 
-for line in open('size_estimation.dat'):
+MAX_LOG_DISTANCE = 140
+PARTIAL = 60
+
+multiplier = pow(2, 160 - MAX_LOG_DISTANCE - 1)
+
+def print_estimation(num_responses_list):
+    avg = float(sum(num_responses_list)) / len(num_responses_list)
+    size = multiplier * avg / 1000000
+    print 'Estimation: %.2f M' % (
+            size)
+    
+
+
+
+
+partial_num_responses = []
+total_num_responses = []
+
+for i, line in enumerate(open('size_estimation.dat')):
     try:
         num_queries, num_responses = [int(x) for x in line.split()]
     except:
         break
-    size = pow(2, 19) * num_responses / 1000000.
-    print '%d queries, %d responses. Estimation: %.2f M' % (
-        num_queries,
-        num_responses,
-        size)
-    sizes.append(size)
+    partial_num_responses.append(num_responses)
+    total_num_responses.append(num_responses)
+    if i % PARTIAL == 0:
+        print_estimation(partial_num_responses)
+        partial_num_responses = []
 
+print len(partial_num_responses),
+print_estimation(partial_num_responses)
         
-avg = sum(sizes)/len(sizes)
-print 'Final estimation: %.2f M' % avg
+print 'Final', 
+print_estimation(total_num_responses)
