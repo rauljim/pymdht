3409671600809a38dd7f609943ec8b0bbb7b0cef
Raul Jimenez <rauljc@kth.se>
2011-03-04 10:29:32 +0100

    changed graph line styles

--- profiler/plotters/plot_cdf_l_time.py
+++ profiler/plotters/plot_cdf_l_time.py
@@ -7,15 +7,15 @@ pylab.ylabel('CDF')
 output_filename = 'plots/cdf_l_time.eps'
 
 lines_to_plot = (
-    ('0.l_time.cdf', 'UT', 'k'),
-    ('1.l_time.cdf', 'NS1', 'r:'),
-    ('2.l_time.cdf', 'NS2', 'b:'),
-    ('3.l_time.cdf', 'NS3', 'g:'),
-    ('4.l_time.cdf', 'NS4', 'y:'),
-    ('5.l_time.cdf', 'NS5', 'c:'),
-    ('6.l_time.cdf', 'NS6', 'r-'),
-    ('7.l_time.cdf', 'NS7', 'b-'),
-    ('8.l_time.cdf', 'NS8', 'g'),
+    ('0.l_time.cdf', '0', 'k'),
+    ('1.l_time.cdf', '1', 'k'),
+    ('2.l_time.cdf', '2', 'k:'),
+    ('3.l_time.cdf', '3', 'k--'),
+    ('4.l_time.cdf', '4', 'r'),
+    ('5.l_time.cdf', '5', 'r:'),
+    ('6.l_time.cdf', '6', 'r-'),
+    ('7.l_time.cdf', '7', 'b-'),
+    ('8.l_time.cdf', '8', 'g'),
 #    ('9.l_time.cdf', 'NS9', 'y'),
     )
 #    (filename, label, style)
