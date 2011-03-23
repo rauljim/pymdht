674486c103860fff49914c03dde1a603df03fe78
Rizvi <ssirizvi@kth.se>
2011-03-23 16:56:19 +0100

    created some new parsers for experiment regarding the oscar event

--- profiler/parsers/get_peers.py
+++ profiler/parsers/get_peers.py
@@ -3,8 +3,7 @@
 # See LICENSE.txt for more information
 
 """
-Prints the lookup time in seconds (one lookup per line).
-The output is not chronologically sorted.
+Prints number of GET_PEERS in seconds, minutes and hours.
 
 """
 from parser_utils import openf
@@ -17,13 +16,19 @@ class Parser(object):
         self.label = label
         self.my_addr = my_addr
 
-        self.cum_gp_file = openf(label + '.cum_gp')
-        self.cum_gp = 0
-       
-        self.sec_gp_file = openf(label + '.sec_gp')
+        self.cum_gp_file = openf(label + '.cumulative_gp')
+        self.sec_gp_file = openf(label + '.per_sec_gp')
+        self.min_gp_file = openf(label + '.per_min_gp')
+        self.hour_gp_file = openf(label + '.per_hour_gp')
+        
+	self.cum_gp = 0
         self.sec_gp = 0
+        self.min_gp = 0
+	self.hour_gp = 0
         self.current_sec = 0
-
+        self.current_min = 0
+	self.current_hour = 0
+    
     def outgoing_msg(self, ts, dst_addr, msg):
         pass
 
@@ -39,16 +44,37 @@ class Parser(object):
             for i in xrange(self.current_sec + 1, int_ts):
                 self.sec_gp_file.write(
                     '%d\t%d\n' % (i, 0))
-                self.sec_gp_file.write(
-                    '%d\t%d\n' % (i, 0))
             self.current_sec = int_ts
             self.sec_gp = 0
-             
+
+	if int(int_ts / 60) > self.current_min:
+            self.min_gp_file.write(
+                '%d\t%d\n' % (self.current_min,
+                              self.min_gp))
+	    for i in xrange(self.current_min + 1, int(int_ts / 60) ):
+		self.min_gp_file.write(
+		    '%d\t%d\n' % (i, 0))
+	    self.current_min = int(self.current_sec / 60)
+            self.min_gp = 0
+
+	if int(int_ts / 3600) > self.current_hour:
+            self.hour_gp_file.write(
+                '%d\t%d\n' % (self.current_hour,
+                              self.hour_gp))
+	    for i in xrange(self.current_hour + 1, int(int_ts / 3600) ):
+		self.hour_gp_file.write(
+		    '%d\t%d\n' % (i, 0))
+	    self.current_hour = int(self.current_sec / 3600)
+            self.hour_gp = 0
+
         if msg.type == message.QUERY:
             if msg.query == message.GET_PEERS:
                 # lookup query
                 self.cum_gp += 1
                 self.sec_gp += 1
+		self.min_gp += 1
+		self.hour_gp += 1
+		pass
             if msg.query == message.ANNOUNCE_PEER:
                 #TODO: announces
                 pass
