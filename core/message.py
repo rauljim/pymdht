1b84b3557a05dd90d9db61fcbb405f4d611b513d
Zinat Sultana <zinat@kth.se>
2011-05-25 19:00:56 +0200

    added comments to start sperimental_module work

--- core/message.py
+++ core/message.py
@@ -119,6 +119,7 @@ class OutgoingQueryBase(OutgoingMsgBase):
         self._dict[TYPE] = QUERY
         self._dict[ARGS] = {ID: str(src_id)}
         self.lookup_obj = None
+        self.experimental_obj = None #zinat
         self.got_response = False
 
     @property
@@ -148,23 +149,25 @@ class OutgoingQueryBase(OutgoingMsgBase):
         
 class OutgoingPingQuery(OutgoingQueryBase):
     
-    def __init__(self, dst_node, src_id):
+    def __init__(self, dst_node, src_id, experimental_obj=None):#zinat
         OutgoingQueryBase.__init__(self, dst_node, src_id)
         self._dict[QUERY] = PING
+        self.experimental_obj = experimental_obj
 
         
 class OutgoingFindNodeQuery(OutgoingQueryBase):
 
-    def __init__(self, dst_node, src_id, target, lookup_obj):
+    def __init__(self, dst_node, src_id, target, lookup_obj, experimental_obj=None):
         OutgoingQueryBase.__init__(self, dst_node, src_id)
         self._dict[QUERY] = FIND_NODE
         self._dict[ARGS][TARGET] = str(target)
         self.lookup_obj = lookup_obj
+        self.experimental_obj = experimental_obj#zinat
 
 
 class OutgoingGetPeersQuery(OutgoingQueryBase):
 
-    def __init__(self, dst_node, src_id, info_hash, lookup_obj):
+    def __init__(self, dst_node, src_id, info_hash, lookup_obj):#zinat:extra param
         OutgoingQueryBase.__init__(self, dst_node, src_id)
         self._dict[QUERY] = GET_PEERS
         self._dict[ARGS][INFO_HASH] = str(info_hash)
