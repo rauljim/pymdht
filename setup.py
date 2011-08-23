0e5c864510922993f9da7496cb0c6ca71998efe9
Raul Jimenez <rauljc@kth.se>
2011-08-23 14:13:24 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='Pymdht',
-    version='1.0',
-    author='Raul Jimenez',
-    author_email='rauljim@gmail.com',
-    packages=['pymdht', 'pymdht.core'],
-    scripts=['bin/interactive_dht.py', 'pymdht-daemon.py'],
+    version='11.8.1',
+    author='Raul Jimenez and others',
+    author_email='rauljc@gkth.se',
+    packages=['pymdht', 'pymdht.core', 'pymdht.plugins',],
+    scripts=['interactive_dht.py', 'pymdht_daemon.py'],
     url='http://pypi.python.org/pypi/Pymdht/',
     license='LICENSE.txt',
     description='A flexible implementation of the Mainline DHT protocol.',
