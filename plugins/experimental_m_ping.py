df74bc36644fec055a20806c5f14912af15a933b
Zinat Sultana <zinat@kth.se>
2011-09-06 17:25:59 +0200

    ommit spaces

--- plugins/experimental_m_ping.py
+++ plugins/experimental_m_ping.py
@@ -22,7 +22,7 @@ class ExperimentalManager:
         
          
     def on_query_received(self, msg):
-        if not self._stop and msg.query =='get_peers':
+        if not self._stop and msg.query =='ping':
            self._stop = True
            self.pinged_ips[msg.src_node.ip] = msg.src_node.ip
            print '\nExperimentalModule got query (%s) from  node  %r =' % (msg.query ,  msg.src_node)
@@ -34,7 +34,6 @@ class ExperimentalManager:
                 #self.pinged_ips[msg.src_node.ip] = True
                 self.pinged_ips[msg.src_node.ip] = STATUS_PINGED
 #                print 'ping send to ip address :  ' , self.pinged_ips['ip_address']
-               
                 return [probe_query]
             
     #return []
@@ -56,9 +55,8 @@ class ExperimentalManager:
             elapsed_time = time.time() - related_query.experimental_obj.query_ts 
             print 'prove FAILED Due to Time-Out' ,related_query.experimental_obj.value
             print 'RTT = ',elapsed_time
-            self.pinged_ips[related_query.dst_node.ip] = STATUS_FAIL
-            self.pinged_ips[related_query.dst_node.ip.return_time] = time.time()
-            
+            self.pinged_ips[related_query.dst_node.ip] = STATUS_FAIL 
+#            
             
                
     def on_stop(self):
@@ -68,7 +66,7 @@ class ExperimentalManager:
         fob=open('c:/Users/zinat/pythonworkspace/pymdht/plugins/ping_res.txt','w')
         for ip, status in self.pinged_ips.iteritems():
             fob.write('%s\t %s\n' % (ip, status))
-        self.fob.close()
+        fob.close()
         
             
 class ExpObj:
